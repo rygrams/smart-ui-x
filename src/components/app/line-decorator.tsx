a573f91215ff716a4e12ca9e990b7e3d50ab8270
Bamory GRAMBOUTE <bamorygramboute@gmail.com>
2025-05-15 21:38:31 +0000

    chore: fix eslint package

--- src/components/app/line-decorator.tsx
+++ src/components/app/line-decorator.tsx
@@ -17,7 +17,7 @@ export interface LineDecoratorCSSProperties extends React.CSSProperties {
 }
 
 export const HorizontalLineDecorator = React.forwardRef<
-  React.ElementRef<'div'>,
+  React.ComponentRef<'div'>,
   LineDecoratorProps
 >(
   (
@@ -45,16 +45,12 @@ export const HorizontalLineDecorator = React.forwardRef<
           ...style,
         } as LineDecoratorCSSProperties
       }
-      // eslint-disable-next-line tailwindcss/no-contradicting-classname
       className={cn(
-        // Line
         'left-[calc(var(--line-offset)/2*-1)]',
         'h-[--line-width] w-[calc(100%+var(--line-offset))]',
         'bg-[linear-gradient(to_right,var(--line-color),var(--line-color)_50%,transparent_0,transparent)]',
         'bg-[size:var(--line-gap)_var(--line-width)]',
-        // Mask / Gradient
         '![mask-composite:exclude] [mask:linear-gradient(to_left,var(--line-background)_var(--line-fade-stop),transparent),linear-gradient(to_right,var(--line-background)_var(--line-fade-stop),transparent),linear-gradient(#000,#000)]',
-        // Animation
         'w-0 animate-landing-horizontal-line-intro',
         className,
       )}
@@ -93,17 +89,12 @@ export const VerticalLineDecorator = React.forwardRef<
           ...style,
         } as LineDecoratorCSSProperties
       }
-      // False positive reported by eslint. For more information, see https://github.com/francoismassart/eslint-plugin-tailwindcss/issues/260.
-      // eslint-disable-next-line tailwindcss/no-contradicting-classname
       className={cn(
-        // Line
         'top-[calc(var(--line-offset)/2*-1)]',
         'h-[calc(100%+var(--line-offset))] w-[--line-width]',
         'bg-[linear-gradient(to_top,var(--line-color),var(--line-color)_50%,transparent_0,transparent)]',
         'bg-[size:var(--line-width)_var(--line-gap)]',
-        // Mask / Gradient
         '![mask-composite:exclude] [mask:linear-gradient(to_left,var(--line-background)_var(--line-fade-stop),transparent),linear-gradient(to_top,var(--line-background)_var(--line-fade-stop),transparent),linear-gradient(#000,#000)]',
-        // Animation
         'h-0 animate-landing-vertical-line-intro',
         className,
       )}
--- src/types/nav.tsx
+++ src/types/nav.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-empty-object-type */
 import { Icons } from '~/components/app/icons'
 
 export interface NavItem {
